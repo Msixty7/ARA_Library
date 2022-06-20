a6b9149e755b90db68e4c9a12dd074eeaff6f792
sgretscher <41306803+sgretscher@users.noreply.github.com>
2022-06-20 20:05:30 +0200

    Hide IPC proxy internal mappings by generalzing the binding call in the IPC and delegating its actual execution to the outer hosting code

--- IPC/ARAIPCEncoding.h
+++ IPC/ARAIPCEncoding.h
@@ -980,6 +980,7 @@ constexpr ARAIPCMessageID _encodeMessageID ()
 constexpr ARAIPCMessageID kGetFactoriesCountMessageID { 1 };
 constexpr ARAIPCMessageID kGetFactoryMessageID { 2 };
 constexpr ARAIPCMessageID kCreateDocumentControllerMessageID { 3 };
+constexpr ARAIPCMessageID kBindToDocumentControllerMessageID { 4 };
 
 
 // caller side: create a message with the specified arguments
